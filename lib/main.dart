2fb8111d836c40dbc38370ca757619843319fe28
Luong Pham <luong.pham@bstarsolutions.com>
2021-03-19 17:07:11 +0700

    sfs

--- lib/main.dart
+++ lib/main.dart
@@ -18,23 +18,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
